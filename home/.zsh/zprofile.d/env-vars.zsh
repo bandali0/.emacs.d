871f3d7c1dcbe64584db8997aa266db94e777deb
Amin Bandali <me@aminb.org>
2014-10-09 20:36:10 -0400

    back to vim

--- home/.zsh/zprofile.d/env-vars.zsh
+++ home/.zsh/zprofile.d/env-vars.zsh
@@ -1,7 +1,7 @@
 export MT_RELEASE_STORE_FILE="../../../Documents/aminbandali-release-keystore.jks"
 export MT_RELEASE_KEY_ALIAS="release"
 
-export EDITOR="emacs -nw"
+export EDITOR="vim"
 export VISUAL=$EDITOR
 
 export `gnome-keyring-daemon --start`
