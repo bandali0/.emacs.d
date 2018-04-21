fd134a2b5330c76b04e8777a8dc001722c706a37
Amin Bandali <amin@aminb.org>
2018-04-20 21:00:30 -0400

    add straight.el, use-package, and no-littering

---
+++ emacs/straight/versions/default.el
@@ -0,0 +1,6 @@
+(("epkgs" . "9a82406a27f95eb551cb5cab74298ac4a58f3e93")
+ ("melpa" . "cb43431d7a7276cdf1ea741b2b218bc46c2722f9")
+ ("no-littering" . "9bffebc0f4858a06ba374f1d48a7dffd3537b93e")
+ ("straight.el" . "23669af11f88eb4f92c2b8882e70921f6503c9f0")
+ ("use-package" . "512aa50b025018cb8574498d868814f73c02bb9b"))
+:jupiter
