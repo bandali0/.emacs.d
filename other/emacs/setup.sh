96361db56c026b405179c98b1dbd58c8a3f2131d
Amin Bandali <amin@aminb.org>
2018-04-29 23:46:44 -0400

    [other] remove some of the out of date stuff
    TODO: Makefile rules for setting up my zim-based zsh setup, along with
    the zsh plugins I use

--- other/emacs/setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# clone my prelude fork
-export PRELUDE_URL="https://github.com/aminb/prelude.git" && \
-    curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
-cd $HOME/.emacs.d
-git remote set-url origin git@github.com:aminb/prelude.git
